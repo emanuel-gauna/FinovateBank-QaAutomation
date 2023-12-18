f27f5088d4a6f68fd275f4dd23e6b29ea960c706
emanuel-gauna <lemagauna@gmail.com>
2023-12-17 21:15:25 -0300

    inicio de proyecto QA automation-finovate-bank

---
+++ tests/login.spec.js
@@ -0,0 +1,22 @@
+
+const { Builder , By } = require("selenium-webdriver");
+const webdriver = require("selenium-webdriver");
+const firefox = require("selenium-webdriver/firefox");
+const edge = require("selenium-webdriver/edge");
+
+describe("Login", ()=>{
+    it("login con contraseña y cierre de sesión", async ()=>{
+   let driver = new webdriver.Builder().forBrowser('chrome').setFirefoxOptions().setEdgeOptions().build();
+    await driver.get('https://finovate-bank.onrender.com/web/pages/login.html')
+    //ingresar usuario y password
+    await driver.findElement(By.id("emailLogin")).sendKeys("lemagauna@gmail.com")
+    await driver.sleep(1000)
+    await driver.findElement(By.id('passwordLogin')).sendKeys('Loquesea2023')
+    //hacer click en el botón de ingresar
+    await driver.findElement(By.className('btn--login')).click();
+   await driver.sleep(9000) 
+   await driver.findElement(By.className('Btn--logout ms-1 ms-md-0')).click()
+   await driver.sleep(5000);
+   await driver.quit(); 
+});
+});  
---
+++ tests/register.spec.js
@@ -0,0 +1,27 @@
+const { Builder, By } = require("selenium-webdriver");
+const  webdriver = require("selenium-webdriver");
+const edge = require("selenium-webdriver/edge");
+const firefox = require("selenium-webdriver/firefox");
+
+describe( "Register", () =>{
+    it("Registro de nuevo usuario y cierre de sesión" , async ()=>{
+        let driver = new webdriver.Builder().forBrowser("chrome").setEdgeOptions().setFirefoxOptions().build();
+        await driver.sleep(2000)
+        await driver.get("https://finovate-bank.onrender.com/web/pages/login.html")
+        await driver.sleep(2000)
+        await driver.findElement(By.className("ball-select")).click()
+        await driver.findElement(By.id("firstName")).sendKeys("usuario")
+        await driver.sleep(2000)
+        await driver.findElement(By.id("lastName")).sendKeys("secreto")
+        await driver.sleep(2000)
+        await driver.findElement(By.id("emailRegister")).sendKeys("User@mail.com")
+        await driver.sleep(2000)
+        await driver.findElement(By.id("passwordRegister")).sendKeys("Secreto2025")
+        await driver.sleep(2000)
+        await driver.findElement(By.xpath('//*[@id="app"]/div/div/div/div/div/div/div/div/div[2]/div/div/button')).click()
+        await driver.sleep(9000)
+        await driver.findElement(By.className('Btn--logout ms-1 ms-md-0')).click()
+        await driver.sleep(5000);
+        await driver.quit(); 
+    })
+}) 
