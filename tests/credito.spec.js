54e23aa9ceae1c2ebd31cc9b604e69c27c6ba40d
Agustín Sabando <agustinsabando18@gmail.com>
2023-12-20 19:52:52 -0300

    Subiendo automatización de transacciones

--- tests/credito.spec.js
+++ tests/credito.spec.js
@@ -1,9 +1,10 @@
 const { Builder, By } = require("selenium-webdriver")
 
 describe('credito', () => {
-    it('debitando dinero de la cuenta de caja de ahorro', async () =>{
+    it('acreditando dinero en la cuenta de caja de ahorro', async () =>{
         let driver = new Builder().forBrowser('chrome').build();
         await driver.get('https://finovate-bank.onrender.com/index.html')
+        //Loguearse en la página web.
         await driver.findElement(By.className('d-flex flex-column align-items-center pt-2 flex-md-row gap-md-3 justify-content-center')).click()
         await driver.findElement(By.id('emailLogin')).sendKeys('agustinsabando18@gmail.com')
         await driver.sleep(3000)
@@ -11,6 +12,7 @@ describe('credito', () => {
         await driver.sleep(3000)
         await driver.findElement(By.className('btn--login mt-4')).click()
         await driver.sleep(3000)
+        //realizando una transferencia de dinero de una cuenta a otra.
         await driver.get('https://finovate-bank.onrender.com/web/pages/transfers.html')
         await driver.sleep(3000)
         await driver.findElement(By.className('radio-button__label')).click()
@@ -25,6 +27,7 @@ describe('credito', () => {
         await driver.sleep(5000)
         await driver.findElement(By.className('swal2-confirm swal2-styled')).click()
         await driver.sleep(5000)
+        //Comprobar que la debitación de dinero esté correcta.
         await driver.get('https://finovate-bank.onrender.com/web/pages/accounts.html')
         await driver.sleep(3000)
         await driver.get('https://finovate-bank.onrender.com/web/pages/account.html?id=271')
--- tests/transacciones.spec.js
+++ tests/transacciones.spec.js
@@ -4,11 +4,13 @@ describe('transacciones', () => {
     it('debitando dinero de la cuenta de caja de ahorro', async () =>{
         let driver = new Builder().forBrowser('chrome').build();
         await driver.get('https://finovate-bank.onrender.com/index.html')
+        //Loguearse en la página web.
         await driver.findElement(By.className('d-flex flex-column align-items-center pt-2 flex-md-row gap-md-3 justify-content-center')).click()
         await driver.findElement(By.id('emailLogin')).sendKeys('agustinsabando18@gmail.com')
         await driver.sleep(3000)
         await driver.findElement(By.id('passwordLogin')).sendKeys('Aguspro345')
         await driver.sleep(3000)
+        //Verificar las transacciones que se realizaron.
         await driver.findElement(By.className('btn--login mt-4')).click()
         await driver.sleep(5000)
         await driver.get('https://finovate-bank.onrender.com/web/pages/account.html?id=271')
