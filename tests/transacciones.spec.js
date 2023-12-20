f15056023867eabac2249041143cfa62adf54c04
Agustín Sabando <agustinsabando18@gmail.com>
2023-12-20 19:52:34 -0300

    Subiendo automatización de transacciones

---
+++ tests/transacciones.spec.js
@@ -0,0 +1,16 @@
+const { Builder, By } = require("selenium-webdriver")
+
+describe('transacciones', () => {
+    it('debitando dinero de la cuenta de caja de ahorro', async () =>{
+        let driver = new Builder().forBrowser('chrome').build();
+        await driver.get('https://finovate-bank.onrender.com/index.html')
+        await driver.findElement(By.className('d-flex flex-column align-items-center pt-2 flex-md-row gap-md-3 justify-content-center')).click()
+        await driver.findElement(By.id('emailLogin')).sendKeys('agustinsabando18@gmail.com')
+        await driver.sleep(3000)
+        await driver.findElement(By.id('passwordLogin')).sendKeys('Aguspro345')
+        await driver.sleep(3000)
+        await driver.findElement(By.className('btn--login mt-4')).click()
+        await driver.sleep(5000)
+        await driver.get('https://finovate-bank.onrender.com/web/pages/account.html?id=271')
+    })
+})
